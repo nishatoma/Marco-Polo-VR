740fbe5d601ae3e446a022a93eb96fc632787253
Nisha Shlaymoon <nishatoma@gmail.com>
2019-03-19 20:15:47 -0400

    Added ripple con collision with the box.

---
+++ Marco Polo/Assets/Scripts/HandController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public GameObject wave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        
+        if (collision.gameObject.tag.Equals("polo")) {
+            // What position do we generate the ripple in?
+            Vector3 wavePosition = collision.contacts[0].point;
+            Debug.Log("Wave position wrtf: " + wavePosition);
+            // Generate at the point of contact with the object.
+            GameObject waveClone = Instantiate(wave, wavePosition, wave.transform.rotation);
+        }
+    }
+}
---
+++ Marco Polo/Assets/Scripts/LightController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] allLights = GameObject.FindGameObjectsWithTag ("dev_light"); 
+         
+        if (Input.GetKeyDown(KeyCode.M)) {
+            foreach (GameObject i in allLights) { 
+                i.SetActive(!i.active); 
+            }
+        }
+    }
+}
