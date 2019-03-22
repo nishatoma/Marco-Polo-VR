91d37ef4d2b7b98ce8f063c8cbfac903340aa2cf
Nisha Shlaymoon <nishatoma@gmail.com>
2019-03-22 17:14:45 -0400

    Added footsteps and revealed player when colliding.

--- Marco Polo/Assets/Scripts/HandController.cs
+++ Marco Polo/Assets/Scripts/HandController.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class HandController : MonoBehaviour
 {
 
-    public enum VibrationForce
-    {
-     Light,
-     Medium,
-     Hard,
-    }
-
-    [SerializeField]
-    OVRInput.Controller controllerMask;
-
     public GameObject wave;
     // Start is called before the first frame update
     void Start()
@@ -29,30 +19,15 @@ public class HandController : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
-        // What position do we generate the ripple in?
-        Vector3 wavePosition = collision.contacts[0].point;
-        Debug.Log("Wave position wrtf: " + wavePosition);
-        // Generate at the point of contact with the object.
-        GameObject waveClone2 = Instantiate(wave, wavePosition, gameObject.transform.rotation);
+        if (collision.gameObject.tag.Equals("polo")) {
+            collision.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            collision.gameObject.transform.GetChild(0).GetComponent<AudioSource>().Stop();
+        } else {
+            // What position do we generate the ripple in?
+            Vector3 wavePosition = collision.contacts[0].point;
+            Debug.Log("Wave position wrtf: " + wavePosition);
+            // Generate at the point of contact with the object.
+            GameObject waveClone2 = Instantiate(wave, wavePosition, gameObject.transform.rotation);
+        }
     }
-
-     void Vibrate(VibrationForce vibrationForce)
-     {
-         var channel = OVRHaptics.RightChannel;
-         if (controllerMask == OVRInput.Controller.LTouch)
-             channel = OVRHaptics.LeftChannel;
- 
-         switch (vibrationForce)
-         {
-             case VibrationForce.Light:
-                 channel.Preempt(clipLight);
-                 break;
-             case VibrationForce.Medium:
-                 channel.Preempt(clipMedium);
-                 break;
-             case VibrationForce.Hard:
-                 channel.Preempt(clipHard);
-                 break;
-         }
-     }
 }
