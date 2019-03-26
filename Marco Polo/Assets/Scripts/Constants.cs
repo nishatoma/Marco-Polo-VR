8d9fad2a9d3bf986454451f7dc2b2e125cacc379
Nisha Shlaymoon <nishatoma@gmail.com>
2019-03-26 12:35:30 -0400

    Add Constants file.
    Keep all the constants like names, tags and numbers in here.

---
+++ Marco Polo/Assets/Scripts/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+Class to keep track of all gameObject names and tags. Any constants go in here.
+ */
+public class Constants
+{
+    // Sound source names
+    public const string SOUND_MARCO = "sound_marco";
+    public const string SOUND_POLO = "sound_polo";
+
+    // Tags
+    public const string TAG_POLO = "polo";
+    public const string TAG_OBSTACLE = "obstacle";
+}
--- Marco Polo/Assets/Scripts/HandController.cs
+++ Marco Polo/Assets/Scripts/HandController.cs
@@ -6,45 +6,27 @@ public class HandController : MonoBehaviour
 {
     public GameObject wave;
     GameObject marcoSound;
-    
-    // Sound source name
-    private const string MARCO = "sound_marco"; 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnCollisionEnter(Collision collision) {
 
-        if (collision.gameObject.tag.Equals("polo")) {
+        if (collision.gameObject.tag.Equals(Constants.TAG_POLO)) {
             collision.gameObject.GetComponent<MeshRenderer>().enabled = true;
             collision.gameObject.transform.GetChild(0).GetComponent<AudioSource>().Stop();
-        } else {
+        } else if (collision.gameObject.tag.Equals(Constants.TAG_OBSTACLE)) {
             createWaveAtCollisionPoint(collision);
             playMarco();
         }
-
-        
     }
 
     void createWaveAtCollisionPoint(Collision collision) {
         // What position do we generate the ripple in?
         Vector3 wavePosition = collision.contacts[0].point;
-        Debug.Log("Wave position wrtf: " + wavePosition);
         // Generate at the point of contact with the object.
         GameObject waveClone2 = Instantiate(wave, wavePosition, gameObject.transform.rotation);
     }
 
     void playMarco() {
-        marcoSound = GameObject.Find(MARCO);
+        marcoSound = GameObject.Find(Constants.SOUND_MARCO);
         marcoSound.GetComponent<AudioSource>().Play();
     }
 }
